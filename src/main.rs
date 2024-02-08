6536fa3e93959986105d8dbcab2ab8177f90c7c9
johnh <john.helliwell@gmail.com>
2024-02-08 16:01:21 +1100

    Merging and deduping of code repitition

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 
-const VERSION: &str = "0.24a";
+const VERSION: &str = "0.25a";
 
 pub mod chevrons;
 pub mod linux;
